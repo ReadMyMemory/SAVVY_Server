87e3fe0e2b97371897abe851f709de78db9e7b9f
ReadMyMemory <lame3925@gmail.com>
2023-07-21 01:34:28 +0900

    Diary : 다이어리 목록 조회 API 완성

--- src/app/Diary/diaryDao.js
+++ src/app/Diary/diaryDao.js
@@ -6,3 +6,24 @@ export const selectDiaryListById = async (connection, user_id) => {
             const diaryListRow = await connection.query(selectDiaryListByIdQuery, user_id);
             return diaryListRow;
 };
+
+export const selectDiarybyId = async (connetion, diary_id) => {
+    const selectDiarybyIdQuery = `
+  SELECT * FROM diary WHERE id = ?;`;
+
+    const diaryRows = await connetion.query(selectDiarybyIdQuery, diary_id);
+    return diaryRows;
+};
+
+export const selectDiaryId = async (connection, user_id) => {
+    const selectDiaryIdQuery = `
+  SELECT id FROM diary WHERE user_id = ?
+  ORDER BY updated_at DESC
+  LIMIT 1;`;
+
+    const selectDiaryIdRow = await connection.query(
+        selectDiaryIdQuery,
+        user_id
+    );
+    return selectDiaryIdRow;
+};
