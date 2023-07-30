e0b8003f5052a2bf8aa8ece33eac403f186f15a4
ReadMyMemory <lame3925@gmail.com>
2023-07-30 17:56:03 +0900

    Update : baseResponse 수정, Add : 답글 작성 시 예외처리 추가

--- src/app/Comment/commentController.js
+++ src/app/Comment/commentController.js
@@ -25,6 +25,11 @@ export const postReply = async (req, res) => {
         const comment_id = req.params.comment_id;
         const user_id = req.verifiedToken.id;
         const contents = req.body.contents;
+
+    // 빈 아이디 체크
+    if (!user_id) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
+    // 빈 댓글 아이디 체크
+    if (!comment_id) return res.send(errResponse(baseResponse.COMMENT_COMMENTID_EMPTY));
         
         const postReplyResponse = await createReply(comment_id, user_id, contents);
         return res.send(postReplyResponse);
