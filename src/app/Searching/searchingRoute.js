448f2045af42b332b16c76481996bdbb4714794b
Soomin <stro200123@gmail.com>
2023-08-02 18:41:49 +0900

    Feat: 사용자 검색 API 추가

--- src/app/Searching/searchingRoute.js
+++ src/app/Searching/searchingRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { jwtMiddleware } from '../../../config/jwtMiddleware';
-import { getDiarySearch } from './searchingController';
+import { getDiarySearch, getUserSearch } from './searchingController';
 
 const searchingRouter = express.Router();
 
 searchingRouter.get('/word', jwtMiddleware, getDiarySearch);
+searchingRouter.get('/user', jwtMiddleware, getUserSearch);
 
 export default searchingRouter;
