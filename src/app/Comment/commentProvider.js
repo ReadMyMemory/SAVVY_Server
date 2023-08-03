376d60483514afc2af5e25b3d2ce06ccc54e23eb
ReadMyMemory <lame3925@gmail.com>
2023-08-04 00:51:16 +0900

    Add : 댓글 조회 시 답글 개수 불러오는 부분 추가

--- src/app/Comment/commentProvider.js
+++ src/app/Comment/commentProvider.js
@@ -5,6 +5,7 @@ import {
   selectCommentbyId,
   selectCommentListbyId,
   selectReplyListbyId,
+  showReplyCountbyId
 } from './commentDao';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration'
@@ -52,7 +53,12 @@ export const retrieveCommentList = async (diary_id, user_id) => {
     diary_id,
     user_id,
   ]);
-
+  if(retrieveCommentListResult[0][0]) {
+    // 답글 수 표시
+    for (let p = 0; p < retrieveCommentListResult.length; p++) {
+      const countValue = await showReplyCountbyId(connection, retrieveCommentListResult[0][p].id);
+      retrieveCommentListResult[0][p].reply_count = countValue[0][0].count;
+    }
   // is_updated의 boolean 치환
   for(let i = 0; i < retrieveCommentListResult[0].length; i ++) {
     if (retrieveCommentListResult[0][i].is_updated === 'true') retrieveCommentListResult[0][i].is_updated = true;
@@ -61,7 +67,7 @@ export const retrieveCommentList = async (diary_id, user_id) => {
     retrieveCommentListResult[0][i].updated_at = await dateDivider(retrieveCommentListResult[0][i].updated_at);
     if(retrieveCommentListResult[0][i].updated_at === -1) retrieveCommentListResult[0][i] = errResponse(baseResponse.TIME_ERROR);
   }
-
+  }
   connection.release();
   return response(baseResponse.SUCCESS, retrieveCommentListResult[0]);
 };
