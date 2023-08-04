a0f5b74ad4a5c179c2b17673a6de2aa08cd394d7
Soomin <stro200123@gmail.com>
2023-08-04 19:58:46 +0900

    Feat: 에러 핸들링 함수 헬퍼함수 추가

--- src/app/Searching/searchingRoute.js
+++ src/app/Searching/searchingRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { jwtMiddleware } from '../../../config/jwtMiddleware';
+import { wrapAsync } from '../../../config/errorHandler';
 import {
   getDiarySearch,
   getUserSearch,
@@ -11,11 +12,15 @@ import {
 
 const searchingRouter = express.Router();
 
-searchingRouter.get('/word', jwtMiddleware, getDiarySearch);
-searchingRouter.get('/user', jwtMiddleware, getUserSearch);
-searchingRouter.get('/word/list', jwtMiddleware, getDiaryHistory);
-searchingRouter.get('/user/list', jwtMiddleware, getUserHistory);
-searchingRouter.delete('/delete', jwtMiddleware, deleteHistory);
-searchingRouter.delete('/delete/all/:type', jwtMiddleware, deleteHistoryAll);
+searchingRouter.get('/word', jwtMiddleware, wrapAsync(getDiarySearch));
+searchingRouter.get('/user', jwtMiddleware, wrapAsync(getUserSearch));
+searchingRouter.get('/word/list', jwtMiddleware, wrapAsync(getDiaryHistory));
+searchingRouter.get('/user/list', jwtMiddleware, wrapAsync(getUserHistory));
+searchingRouter.delete('/delete', jwtMiddleware, wrapAsync(deleteHistory));
+searchingRouter.delete(
+  '/delete/all/:type',
+  jwtMiddleware,
+  wrapAsync(deleteHistoryAll)
+);
 
 export default searchingRouter;
